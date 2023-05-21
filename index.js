2583ea96557cba4e2861ee97c12610871ac2146f
Fweej <praneethr.github@gmail.com>
2023-05-21 14:39:41 -0400

    index.js

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
-console.log('Saraayu');
+const { app, BrowserWindow } = require('electron');
+const path = require('path');
+
+function createWindow() {
+   const win = new BrowserWindow({
+      width: 1920,
+      height: 1080,
+      minWidth: 1920,
+      minHeight: 1080,
+      webPreferences: {
+         nodeIntegration: true,
+         contextIsolation: false,
+         preload: path.join(__dirname, './homePreload.js')
+      },
+      title: 'OneLink'
+   });
+   win.loadFile(path.join(__dirname, './home.html'));
+}
+
+app.whenReady().then(() => {
+   createWindow();
+   app.on('activate', () => {
+      if (BrowserWindow.getAllWindows().length === 0) createWindow();
+   });
+});
+
+app.on('window-all-closed', () => {
+   if (process.platform !== 'darwin') app.quit();
+});
